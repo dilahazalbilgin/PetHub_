f1f9036d8043f234655ae793e9149b7f1188af8d
ecesariyerr <ecesariyerr@gmail.com>
2024-05-09 22:32:17 +0300

    interface and other pages arranged

--- PetHub/src/pethub/Animals.java
+++ PetHub/src/pethub/Animals.java
@@ -1,7 +1,22 @@
 package pethub;
 import java.util.Scanner;
-public abstract class Animals {
 
+public abstract class Animals {
+    protected String animalSpecies;
+    protected int animalAge;
+    protected String animalDetails;
+    protected String animalStories;
+    protected boolean reproductive;
+    
+    public Animals(String animalSpecies, int animalAge, String animalDetails, String animalStories) {
+        this.animalSpecies = animalSpecies;
+        this.animalAge = animalAge;
+        this.animalDetails = animalDetails;
+        this.animalStories = animalStories;
+    }
+    
+    public abstract void makeSound();
+    
     public String getAnimalSpecies() {
         return animalSpecies;
     }
@@ -33,45 +48,42 @@ public abstract class Animals {
     public void setAnimalStories(String animalStories) {
         this.animalStories = animalStories;
     }
-    protected String animalSpecies;
-    protected int animalAge;
-    protected String animalDetails;
-    protected String animalStories;
-    
-    public Animals(String animalSpecies,int animalAge,String animalDetails,String animalStories){
-        this.animalSpecies=animalSpecies;
-        this.animalAge=animalAge;
-        this.animalDetails=animalDetails;
-        this.animalStories=animalStories;
+
+    public void wantsAnimal(){
+        System.out.println("Which animal do you want to adopt? :");
+        Scanner keyb=new Scanner(System.in);
+        System.out.println("1-Dog\n2-Cat\n3-Bird\n4-Fish");
+        int n1=keyb.nextInt();
+        if(n1==1){      
+        }
     }
-    public abstract void makeSound();
+
+    public boolean isReproductive() {
+        return reproductive;
+    }
+
+    public void setReproductive(boolean reproductive) {
+        this.reproductive = reproductive;
+    }
+
     public void AddType(){
         System.out.println("You must add a type");
         Scanner keyb=new Scanner(System.in);
         String type=keyb.nextLine();
         System.out.println("Type: " +type);
     }
+
     public void AddAge(){
-        System.out.println("You must add a age");
+        System.out.println("You must add an age");
         Scanner keyb=new Scanner(System.in);
         int age=keyb.nextInt();
         System.out.println("Age: " +age);
     }
+
     public void AddStory(){
         System.out.println("You must add a story");
         Scanner keyb=new Scanner(System.in);
         String story=keyb.nextLine();
         System.out.println("Story: " +story);
     }
-    public void wantsAnimal(){
-        System.out.println("Which animal do you want to adopt? :");
-        Scanner keyb=new Scanner(System.in);
-        System.out.println("1-Dog\n2-Cat\n3-Bird\n4-Fish");
-        int n1=keyb.nextInt();
-        if(n1==1){
-            
-        }else if(n1==2){
-        }else if(n1==3){
-        }else if(n1==4){
-        }else{System.out.println("We don't have this animal type");}
-}}
+}

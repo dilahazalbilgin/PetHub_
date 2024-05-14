ce0db32528ae0c4ba9fe7ba33ab95745643e00b4
dilahazalbilgin <dilahazalbilgin@gmail.com>
2024-05-14 10:49:41 +0300

    sqldneme

---
+++ PetHub/src/pethub/NewClass2.java
@@ -0,0 +1,4 @@
+package pethub;
+public class NewClass2 {
+    
+}
--- PetHub/src/pethub/PetHub.java
+++ PetHub/src/pethub/PetHub.java
@@ -8,7 +8,8 @@ public class PetHub {
 
     public static void main(String[] args) {
         System.out.println("WELCOME TO PETHUB!!! \nIF YOU HAVE AN ACCOUNT, PRESS 1 AND LOG IN \nIF YOU DON'T HAVE AN ACCOUNT, PRESS 2 AND SIGN IN!");
-
+        NewClass.connect();
+        NewClass1.createNewDatabase("61");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
         scanner.nextLine(); // Boş satırı tüket

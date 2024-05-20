2e60ddbb91f299201147f0d3fa60236fcc1dbe7b
ecesariyerr <ecesariyerr@gmail.com>
2024-05-20 21:23:22 +0300

    sql

--- PetHub/src/pethub/CreateTable.java
+++ PetHub/src/pethub/CreateTable.java
@@ -9,7 +9,7 @@ public class CreateTable {
     public static void createNewTable() {  
         String url = "jdbc:sqlite:C:/sqlite/SSSIT.db";  
           
-        String sql = "CREATE TABLE IF NOT EXISTS User (\n"  
+        String userTable = "CREATE TABLE IF NOT EXISTS User (\n"  
                 + " id INTEGER PRIMARY KEY,\n"  
                 + " name TEXT NOT NULL,\n"  
                 + " surname TEXT NOT NULL,\n"
@@ -17,11 +17,27 @@ public class CreateTable {
                 + " phoneNumber TEXT NOT NULL,\n"
                 + " password TEXT NOT NULL\n"
                 + ");";  
+        
+        String animalTable = "CREATE TABLE IF NOT EXISTS Animal(\n"
+                + " id INTEGER PRIMARY KEY,\n"
+                + " name TEXT NOT NULL,\n"
+                + " age INTEGER,\n"
+                + " animalSpecies TEXT NOT NULL,\n"
+                + " reproductive TEXT NOT NULL,\n"
+                + " gender TEXT NOT NULL,\n"
+                + " owner_id INTEGER,\n"
+                + " FOREIGN KEY (owner_id) REFERENCES User(id)\n"
+                + ");";
           
         try{  
             Connection conn = DriverManager.getConnection(url);  
             Statement stmt = conn.createStatement();  
-            stmt.execute(sql);  
+            stmt.execute(userTable); 
+            stmt.execute(animalTable);
         } catch (SQLException e) {  
             System.out.println(e.getMessage());  
-        }}}
+        }}
+
+
+
+}

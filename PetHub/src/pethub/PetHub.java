50229d805344efb7444024ff8af886b2ba2c26cf
dilahazalbilgin <dilahazalbilgin@gmail.com>
2024-05-21 13:02:25 +0300

    logın deneme

--- PetHub/src/pethub/PetHub.java
+++ PetHub/src/pethub/PetHub.java
@@ -9,6 +9,8 @@ public class PetHub {
     public static void main(String[] args) {
         System.out.println("WELCOME TO PETHUB!!! \nIF YOU HAVE AN ACCOUNT, PRESS 1 AND LOG IN \nIF YOU DON'T HAVE AN ACCOUNT, PRESS 2 AND SIGN IN!");
         
+        //LogIn u1=new LogIn();        
+        
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
         scanner.nextLine(); 

945b004415a7d1f7c4fbe3c1013f3489411d78be
ecesariyerr <ecesariyerr@gmail.com>
2024-05-30 09:14:35 +0300

    sql

--- PetHub/src/pethub/DataBaseOperation.java
+++ PetHub/src/pethub/DataBaseOperation.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;  
 import java.sql.Statement;
    
-public class DataBaseOperation{    
+public class DataBaseOperation {    
     
     public Connection conn = null;  
     String url = "jdbc:sqlite:C:/sqlite/SSSIT.db"; 
@@ -23,7 +23,8 @@ public class DataBaseOperation{
         }
         return conn;
     }
-     public void insertUser(String name, String surname, String username, String phoneNumber, String password) {
+    
+    public void insertUser(String name, String surname, String username, String phoneNumber, String password) {
         String sql = "INSERT INTO User(name, surname, username, phoneNumber, password) VALUES(?,?,?,?,?)";
 
         try (Connection conn = this.connect(); 
@@ -55,12 +56,12 @@ public class DataBaseOperation{
         }
     }
 
-    public void insertPost(String yes_no, String whichAnimal, String why, String addDetail) {
-        String sql = "INSERT INTO Post(yes_no, which_animal, why, add_detail) VALUES(?,?,?,?)";
+    public void insertPost(String see_add, String whichAnimal, String why, String addDetail) {
+        String sql = "INSERT INTO Post(see_add, which_animal, why, add_detail) VALUES(?,?,?,?)";
 
         try (Connection conn = this.connect(); 
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pstmt.setString(1, yes_no);
+            pstmt.setString(1, see_add);
             pstmt.setString(2, whichAnimal);
             pstmt.setString(3, why);
             pstmt.setString(4, addDetail);
@@ -69,14 +70,14 @@ public class DataBaseOperation{
             System.out.println(e.getMessage());
         }
     }
-    public void selectAllUsers(){  
+
+    public void selectAllUsers() {  
         String sql = "SELECT * FROM User";  
           
         try {  
             this.conn = DriverManager.getConnection(url);  
-            Statement stmt  = conn.createStatement();  
-            ResultSet rs    = stmt.executeQuery(sql);  
-              
+            Statement stmt = conn.createStatement();  
+            ResultSet rs = stmt.executeQuery(sql);  
               
             while (rs.next()) {  
                 System.out.println(rs.getInt("id") +  "\t" +   
@@ -91,15 +92,13 @@ public class DataBaseOperation{
         }  
     }
 
-    
     public void selectAllAnimals() {  
         String sql = "SELECT * FROM Animal";  
           
         try {  
             this.conn = DriverManager.getConnection(url);  
-            Statement stmt  = conn.createStatement();  
-            ResultSet rs    = stmt.executeQuery(sql);  
-              
+            Statement stmt = conn.createStatement();  
+            ResultSet rs = stmt.executeQuery(sql);  
               
             while (rs.next()) {  
                 System.out.println(rs.getInt("id") +  "\t" +   
@@ -113,61 +112,59 @@ public class DataBaseOperation{
             System.out.println(e.getMessage());  
         }  
     }
-    
-public void selectAnimalBySpecies(String species) {  
-    String sql = "SELECT * FROM Animal WHERE animalSpecies = '" + species + "'";  
-      
-    try {  
-        this.conn = DriverManager.getConnection(url);  
-        Statement stmt  = conn.createStatement();  
-        ResultSet rs    = stmt.executeQuery(sql);  
-          
-        while (rs.next()) {  
-            System.out.println(rs.getInt("id") +  "\t" +   
-                               rs.getString("name") + "\t" +  
-                               rs.getInt("age") + "\t" +
-                               rs.getString("animalSpecies") + "\t" +
-                               rs.getString("reproductive") + "\t" +
-                               rs.getString("gender") + "\t"
-                              );
-        }  
-    } catch (SQLException e) {  
-        System.out.println(e.getMessage());  
-    }  
-}
 
-public void selectPostBySpecies(String species) {  
-    String sql = "SELECT * FROM Post WHERE which_animal = '" + species + "'";  
-      
-    try {  
-        this.conn = DriverManager.getConnection(url);  
-        Statement stmt  = conn.createStatement();  
-        ResultSet rs    = stmt.executeQuery(sql);  
+    public void selectAnimalBySpecies(String species) {  
+        String sql = "SELECT * FROM Animal WHERE animalSpecies = '" + species + "'";  
           
-        while (rs.next()) {  
-        System.out.println(rs.getInt("id") +  "\t" +   
-                                   rs.getString("yes_no") + "\t" +  
+        try {  
+            this.conn = DriverManager.getConnection(url);  
+            Statement stmt = conn.createStatement();  
+            ResultSet rs = stmt.executeQuery(sql);  
+              
+            while (rs.next()) {  
+                System.out.println(rs.getInt("id") +  "\t" +   
+                                   rs.getString("name") + "\t" +  
+                                   rs.getInt("age") + "\t" +
+                                   rs.getString("animalSpecies") + "\t" +
+                                   rs.getString("reproductive") + "\t" +
+                                   rs.getString("gender") + "\t");
+            }  
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
+        }  
+    }
+
+    public void selectPostBySpecies(String species) {  
+        String sql = "SELECT * FROM Post WHERE which_animal = '" + species + "'";  
+          
+        try {  
+            this.conn = DriverManager.getConnection(url);  
+            Statement stmt = conn.createStatement();  
+            ResultSet rs = stmt.executeQuery(sql);  
+              
+            while (rs.next()) {  
+                System.out.println(rs.getInt("id") +  "\t" +   
+                                   rs.getString("see_add") + "\t" +  
                                    rs.getString("which_animal") + "\t" +
                                    rs.getString("why") + "\t" +
                                    rs.getString("add_detail") + "\t");
+            }  
+        } catch (SQLException e) {  
+            System.out.println(e.getMessage());  
         }  
-    } catch (SQLException e) {  
-        System.out.println(e.getMessage());  
-    }  
-}
- 
+    }
+
     public void selectAllPosts() {  
         String sql = "SELECT * FROM Post";  
           
         try {  
             this.conn = DriverManager.getConnection(url);  
-            Statement stmt  = conn.createStatement();  
-            ResultSet rs    = stmt.executeQuery(sql);  
+            Statement stmt = conn.createStatement();  
+            ResultSet rs = stmt.executeQuery(sql);  
               
-            // loop through the result set  
             while (rs.next()) {  
                 System.out.println(rs.getInt("id") +  "\t" +   
-                                   rs.getString("yes_no") + "\t" +  
+                                   rs.getString("see_add") + "\t" +  
                                    rs.getString("which_animal") + "\t" +
                                    rs.getString("why") + "\t" +
                                    rs.getString("add_detail") + "\t");

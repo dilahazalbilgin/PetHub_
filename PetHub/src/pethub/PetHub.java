e535f665992091a534d474dc5394f8e2a0d991cd
dilahazalbilgin <dilahazalbilgin@gmail.com>
2024-06-24 23:59:07 +0300

    update

--- PetHub/src/pethub/PetHub.java
+++ PetHub/src/pethub/PetHub.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PetHub {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO PETHUB!!! \nIF YOU HAVE AN ACCOUNT, PRESS 1 AND LOG IN \nIF YOU DON'T HAVE AN ACCOUNT, PRESS 2 AND SIGN IN!");
+        System.out.println("WELCOME TO PETHUB!!! \nIF YOU HAVE AN ACCOUNT, PRESS 1 AND LOG IN \nIF YOU DON'T HAVE AN ACCOUNT, PRESS 2 AND SIGN IN!! ");
 
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
--- PetHub/src/pethub/User.java
+++ PetHub/src/pethub/User.java
@@ -11,6 +11,7 @@ public class User {
         this.surname = surname;
         this.phoneNumber = phoneNumber;       
     }          
+    
         //User Information
         public void Information(){
         System.out.println("Your name: " + name);
